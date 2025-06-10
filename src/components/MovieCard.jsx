46a85e8124a8785bd903f7ff59c0069769abf757
Saurabh Singh <dashingsaurabh1996@gmail.com>
2025-06-11 03:00:16 +0530

    Added details section for movies

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import { DEFAULT_POSTER } from '../../constant'
+import { useNavigate } from 'react-router-dom'
 
 const MovieCard = ({ movie }) => {
     // default poster
     if(movie.Poster === "N/A") {
         movie.Poster = DEFAULT_POSTER
     }
+
+    // navigate to movie details
+    const navigate = useNavigate()
+    const handleMovieDetails = () => {
+        navigate(`/movie/${movie.imdbID}`)
+    }
+
     return (
-        <div className='bg-gray-800 rounded-lg overflow-hidden shadow-lg transform transition duration-300 hover:scale-105 hover:shadow-2xl'>
+        <div className='bg-gray-800 rounded-lg overflow-hidden shadow-lg transform transition duration-300 hover:scale-105 hover:shadow-2xl' onClick={handleMovieDetails}>
             <div className='h-48 bg-gradient-to-r from-purple-500 to-pink-500' style={{ backgroundImage: `url(${movie.Poster || DEFAULT_POSTER})` }}></div>
             <div className='p-6'>
                 <h2 className='text-2xl font-bold text-white mb-2'>{movie.Title}</h2>
