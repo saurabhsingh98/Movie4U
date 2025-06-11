f2656f117e59d6bcf4845738d34c7d9bbdfa98e5
Saurabh Singh <dashingsaurabh1996@gmail.com>
2025-06-11 14:21:49 +0530

    Updated Footer for better design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Footer() {
   return (
     <footer className='bg-gray-900 text-white py-6'>
       <div className='container mx-auto px-4'>
-        <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-6 justify-center'>
           {/* Main Info */}
           <div>
             <h3 className='text-xl font-bold mb-2'>Movies4U</h3>
@@ -12,12 +13,13 @@ export default function Footer() {
           </div>
 
           {/* Quick Links */}
-          <div className='flex flex-col gap-1'>
-            <h3 className='text-xl font-bold mb-2'>Quick Links</h3>
-            <div className='flex gap-1'>
-              <a href="/" className='text-gray-400 hover:text-white'>Home</a>
-              <a href="/about" className='text-gray-400 hover:text-white'>About</a>
-              <a href="/terms" className='text-gray-400 hover:text-white'>Terms</a>
+          {/* move contents to end of div */}
+          <div className='flex flex-col gap-2 justify-end items-end'>
+            <h3 className='text-xl font-bold mb-0 text-center'>Quick Links</h3>
+            <div className='flex gap-2 justify-center'>
+              <Link to="/" className='text-gray-400 hover:text-white'>Home</Link>
+              <Link to="/about" className='text-gray-400 hover:text-white'>About</Link>
+              <Link to="/terms" className='text-gray-400 hover:text-white'>Terms</Link>
             </div>
           </div>
         </div>
